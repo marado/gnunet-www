dbf11d7daf526574bf2f7c9ef8cbca7ce56ae635
tg(x) <*@tg-x.net>
2017-01-19 19:46:02 +0100

    i18n: fix common includes

--- template.sh
+++ template.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
-for f in $(git ls-files *.j2 common/*.j2); do
+for f in $(git ls-files *.j2); do
     for ld in locale/*/; do
 	l=$(basename $ld)
 	echo "$f: $l"
-	python template.py $f $l $(basename $f .html.j2).$l.html
+	python template.py $f $l $(dirname $f)/$(basename $f .html.j2).$l.html
+    done
+done
+
+for f in $(git ls-files common/*.j2); do
+    for ld in locale/*/; do
+	l=$(basename $ld)
+	echo "$f: $l"
+	python template.py $f $l $(dirname $f)/$(basename $f .inc.j2).$l.inc
     done
 done
