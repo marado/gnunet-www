592850e144e38520fb166ee490c25b0283257d16
tg(x) <*@tg-x.net>
2017-01-19 23:45:29 +0100

    i18n: fix js, lang switcher

--- menu.js
+++ menu.js
@@ -24,54 +24,6 @@
 
 // @license magnet:?xt=urn:btih:1f739d935676111cfff4b4693e3816e664797050&dn=gpl-3.0.txt GPL-v3-or-Later
 
-function supports_html5_storage() {
-	try {
-		return 'sessionStorage' in window && window['sessionStorage'] !== null;
-	} catch (e) {
-		return false;
-	}
-}
-
-function setLang(l)
-{
-	document.body.className=l;
-	document.body.parentNode.setAttribute('lang', l);
-
-	if (supports_html5_storage()) {
-		sessionStorage.setItem('lang', l);
-	}
-	if (l == 'en') document.title = 'GNU Taler - Taxable Anonymous Libre Electronic Reserve';
-	if (l == 'de') document.title = 'GNU Taler - Taxierbare Anonyme Liberale Elektronische Reserven';
-	if (l == 'fr') document.title = 'GNU Taler - Taxable Anonyme Libre Électronique Réserve';
-	if (l == 'it') document.title = 'GNU Taler';
-}
-
-function get_default_lang()
-{
-        langs = ['en', 'de', 'fr', 'it'];
-
-	for (var i in navigator.languages) {
-		for (var j in langs) {
-			if (navigator.languages[i].indexOf(langs[j]) != -1) {
-				return langs[j];
-			}
-		}
-	}
-
-	return 'en';
-}
-
-function loadLang()
-{
-	l = 'null';
-	if (supports_html5_storage()) {
-		l = sessionStorage.getItem('lang');
-	}
-
-	if (l == 'null') l = get_default_lang();
-	setLang(l);
-}
-
 function activate_menu()
 {
 	b = document.getElementsByClassName("m_" + document.URL.replace(/^.*(\\|\/|\:)/, '').split('.')[0])
