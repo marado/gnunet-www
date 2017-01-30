d5f793bcd9f47392ee6e61c27f65fa85cbcc5176
Florian Dold <florian.dold@gmail.com>
2017-01-30 21:33:46 +0100

    remove more ssi

--- template.py
+++ template.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import re
 import gettext
 import jinja2
 
@@ -10,7 +11,8 @@ if len(sys.argv) < 3:
 
 in_file = sys.argv[1]
 locale = sys.argv[2]
-out_file = sys.argv[3]
+
+name, ext = re.match(r"(.*)\.([^.]+)$", in_file.rstrip(".j2")).groups()
 
 tr = gettext.translation("messages",
                          localedir="locale",
@@ -23,12 +25,19 @@ env.install_gettext_translations(tr, newstyle=True)
 
 tmpl = env.get_template(in_file)
 
+def self_localized(x):
+    return ".".join((name, x, ext))
+
+def url_localized(my_file):
+    my_name, my_ext = re.match(r"(.*)\.([^.]+)$", my_file).groups()
+    return ".".join((my_name, locale, my_ext))
+
 def url(x):
     # TODO: look at the app root environment variable
     # TODO: check if file exists
     return x
 
 import codecs
-f = codecs.open(out_file, "w", "utf-8")
-f.write(tmpl.render(lang=locale, url=url))
+f = codecs.open(".".join((name, locale, ext)), "w", "utf-8")
+f.write(tmpl.render(lang=locale, url=url, self_localized=self_localized, url_localized=url_localized))
 f.close()
--- template.sh
+++ template.sh
@@ -4,6 +4,6 @@ for f in $(git ls-files *.j2); do
     for ld in locale/*/; do
 	l=$(basename $ld)
 	echo "$f: $l"
-	python template.py $f $l $(dirname $f)/$(basename $f .html.j2).$l.html
+	python template.py $f $l
     done
 done
