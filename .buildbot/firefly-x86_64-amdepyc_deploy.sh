3eb525bcbf11372001baa05c6a7288adf03e1afb
Martin Schanzenbach <mschanzenbach@posteo.de>
2021-08-30 19:11:24 +0200

    -update deployment

--- .buildbot/firefly-x86_64-amdepyc_deploy.sh
+++ .buildbot/firefly-x86_64-amdepyc_deploy.sh
@@ -5,7 +5,7 @@
 chmod -R ag+rX rendered/
 DEPLOY_USER="www"
 if [ $(git rev-parse --abbrev-ref HEAD) == 'stable' ]; then
-  rsync -a --delete rendered/ $DEPLOY_USER@gnunet.org:~/www_deployment/
+  rsync -a --delete rendered/ $DEPLOY_USER@firefly.gnunet.org:~/stable/
 else
   rsync -a --delete rendered/ $DEPLOY_USER@firefly.gnunet.org:~/stage/
 fi
